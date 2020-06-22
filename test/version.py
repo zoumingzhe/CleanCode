0cd483f1b022446bb393a3c91c1d3e81df77dd03
邹明哲 <zoumingzhe@qq.com>
2020-06-22 15:45:17 +0800

    add test file

---
+++ test/version.py
@@ -0,0 +1,13 @@
+﻿import sys
+sys.path.append(r'.\CleanCode\CleanCode')
+import clean
+import scan
+
+
+clean = clean.clean()
+clean.version(isShow = True)
+input("按回车（Enter）继续")
+
+scan = scan.scan()
+scan.version(isShow = True)
+input("按回车（Enter）继续")
