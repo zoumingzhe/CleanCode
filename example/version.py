f53af9e6f506547c6f7837d6a2a32bb7262a9bd1
邹明哲 <zoumingzhe@qq.com>
2020-06-22 14:28:28 +0800

    add example
    version show

---
+++ example/version.py
@@ -0,0 +1,10 @@
+﻿import sys
+import CleanCode
+
+scan = CleanCode.scan()
+scan.version(isShow = True)
+input("按回车（Enter）继续")
+
+clean = CleanCode.clean()
+clean.version(isShow = True)
+input("按回车（Enter）继续")
