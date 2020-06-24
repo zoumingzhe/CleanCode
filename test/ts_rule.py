067b8f5e12b847b83d9c47eb406e3bcb29836738
邹明哲 <zoumingzhe@qq.com>
2020-06-24 11:42:34 +0800

    add rule

---
+++ test/ts_rule.py
@@ -0,0 +1,10 @@
+﻿import sys
+sys.path.append(r'..\CleanCode\CleanCode')
+import rule
+
+
+rule = rule.rule()
+rule.version(isShow = True)
+
+rule.analyse(r'.\case\rule0001.ini')
+input("按回车（Enter）继续")
